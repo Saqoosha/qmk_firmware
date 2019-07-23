e088b4aa3c102f891681b8c037c9f306ebd04dfa
Saqoosha <a@saqoo.sh>
2019-07-23 10:46:33 +0900

    Master is right

--- keyboards/helix/rev2/keymaps/saqoosha/config.h
+++ keyboards/helix/rev2/keymaps/saqoosha/config.h
@@ -21,6 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 // place overrides here
+#define MASTER_RIGHT
 
 // If you need more program area, try select and reduce rgblight modes to use.
 
